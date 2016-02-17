d0c678ab47dcdc9721060065c22a53bc39ac9925
blackhacker666 <saeidzarein@yahoo.com>
2016-02-16 22:53:58 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -227,7 +227,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {146340607},--Sudo users
+    sudo_users = {164293448},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[SPARTACUS Robot Ver 3.7
